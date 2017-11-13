d8889ca84f2530b2f73455ca47d7086e2ad767a1
Nikola V <vukovicnikola@gmail.com>
2017-11-13 12:39:14 +0100

    modified output filename

--- Run_SpaceNetClassifier.py
+++ Run_SpaceNetClassifier.py
@@ -14,8 +14,7 @@ from sklearn.externals import joblib
 # Go to project working directory (file paths will be relative)
 os.chdir("/projects/MINDLAB2016_TMS-NovelWordKurtosis/scratch/MVPA/")
 
-# Load the MNI template brain, and the corresponding masks
-mnibrain = './mni152_template/mni_152_t1_brain.nii'
+# Load the MNI masks
 mnibrainmask = './mni152_template/30PercentMasksResampled/rmni_152_t1_mask.nii'
 mnigm = './mni152_template/30PercentMasksResampled/rmni_152_gm_30mask.nii'
 mniwm = './mni152_template/30PercentMasksResampled/rmni_152_wm_30mask.nii'
@@ -35,8 +34,9 @@ testday = 1 # select data from day 1 or 2
 excludegroup = 3 # analyse groups 1 and 2
 num_folds = 10 # number of KFolds
 fitpenalty = 'tv-l1' # regularisation using tv-l1 or graph-net
-maskname = 'GM' # mask name string
-mnimask = mnigm # mask data for model fit: mnigm, mniwm, mnicsf, mnibrainmask
+screenp = 20 # screening percentile, default 20
+maskname = 'GM30' # mask name string
+mnimask = mnigm # mask data for model fit: mnigm, mniwm, mnibrainmask
 
 logging.info('Parameters: testday @testday, fit @fitpenalty, mask @maskname')
 ######################################################################
@@ -67,7 +67,7 @@ logging.info('Cross validation set up.')
 # 3. CREATE AND FIT MODEL
 # Define SpaceNetClassifier with tv-l1/graph-net penalty
 decoder = SpaceNetClassifier(cv=cv, memory="nilearn_cache", penalty=fitpenalty,
-                            memory_level=2, n_jobs=1, verbose=2, screening_percentile=100, mask=mnimask)
+                            memory_level=2, n_jobs=1, verbose=2, screening_percentile=screenp, mask=mnimask)
 logging.info('Classifier set up. Starting model fit.')
 decoder.fit(X, y)  # fit
 logging.info('Model fit complete. Saving outputs...')
@@ -77,13 +77,13 @@ logging.info('Model fit complete. Saving outputs...')
 # Save decoder object
 decoder_dir = './decoder/' # directory
 if not os.path.exists(decoder_dir): os.makedirs(decoder_dir) # create if missing
-dec_filename = '%s%s_decoder_SpaceNet_%s_day%d_%s.jbl' %(decoder_dir,timestamp, decoder.penalty, testday, maskname)
+dec_filename = '%s%s_decoder_SpaceNet_%sscreen%d_day%d_%s.jbl' %(decoder_dir,timestamp, decoder.penalty, screenp, testday, maskname)
 joblib.dump(decoder, dec_filename)
 
 # Save coefficients to nifti file
 coef_dir = './coefs/' # directory
 if not os.path.exists(coef_dir): os.makedirs(coef_dir) # create if missing
-coef_filename = '%s%s_coefs_SpaceNet_%s_day%d_%s.nii' %(coef_dir,timestamp, decoder.penalty, testday, maskname)
+coef_filename = '%s%s_coefs_SpaceNet_%sscreen%d_day%d_%s.nii' %(coef_dir,timestamp, decoder.penalty, screenp, testday, maskname)
 coef_img = decoder.coef_img_
 coef_img.to_filename(coef_filename)
 logging.info('All outputs saved.')
