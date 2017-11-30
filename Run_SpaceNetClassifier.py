fac027cfc807d11d4ae0af845e68ac4afa9721ea
Nikola V <vukovicnikola@gmail.com>
2017-11-30 12:21:00 +0100

    fixed pep8 format

--- Run_SpaceNetClassifier.py
+++ Run_SpaceNetClassifier.py
@@ -1,4 +1,4 @@
-# DKI-TMS Word Learning Study
+"""DKI-TMS Word Learning Study."""
 # TODO: Describe study design and data structure etc.
 
 # IMPORTS
@@ -22,30 +22,32 @@ mniwm = './mni152_template/30PercentMasks/mni_152_wm30_1mm.nii'
 # SETUP LOGGING
 # Get timestamp as string
 timestamp = time.strftime('%Y%m%d-%H%M%S')
-logname = './%s_SpaceNet_log.txt' %(timestamp)
-logging.basicConfig(filename=logname,
-                    level=logging.INFO,
-                    format='%(asctime)s - %(levelname)s : %(message)s',
-                    datefmt='%d %b %Y %H:%M:%S')
+logname = './%s_SpaceNet_log.txt' % (timestamp)
+logging.basicConfig(
+    filename=logname,
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s : %(message)s',
+    datefmt='%d %b %Y %H:%M:%S')
 
 # 0. ANALYSIS PARAMETERS
-inputcsv = 'MDinfo.csv' # csv containing participant info and file paths
-testday = 1 # select data from day 1 or 2
-excludegroup = 3 # analyse groups 1 and 2
-num_folds = 10 # number of KFolds
-fitpenalty = 'tv-l1' # regularisation using tv-l1 or graph-net
-screenp = 20 # screening percentile, default 20
-maskname = 'GM30_1mm' # mask name string
-mnimask = mnigm # mask data for model fit: mnigm, mniwm, mnibrainmask
+inputcsv = 'MDinfo.csv'  # csv containing participant info and file paths
+testday = 1  # select data from day 1 or 2
+excludegroup = 3  # analyse groups 1 and 2
+num_folds = 10  # number of KFolds
+fitpenalty = 'tv-l1'  # regularisation using tv-l1 or graph-net
+screenp = 20  # screening percentile, default 20
+maskname = 'GM30_1mm'  # mask name string
+mnimask = mnigm  # mask data for model fit: mnigm, mniwm, mnibrainmask
 
-logging.info('Params: testday %s, fit %d, folds %d, screen %d, maskname %s, mask %s',
-                testday, fitpenalty, num_folds, screenp, maskname, mnimask)
+logging.info(
+    'Params: testday %s, fit %d, folds %d, screen %d, maskname %s, mask %s',
+    testday, fitpenalty, num_folds, screenp, maskname, mnimask)
 ######################################################################
 
 # 1. LOAD THE DATA
 # Load csv
 inputdata = pd.read_csv(inputcsv)
-# Select a subset of data based on testing day and tms group (1=M1,2=SPL,3=M1Control)
+# Select data based on testing day & tms group (1=M1,2=SPL,3=M1Control)
 datasubset = inputdata.query('day==@testday & tms!=@excludegroup')
 # Get array X of .nii file paths from the "image" column
 X = np.array(datasubset['image'])
@@ -61,14 +63,21 @@ X = X[perm]
 
 # 2. CONSTRUCT TRAINING & TEST DATA
 # Use StratifiedKFold cross-validation generator: 10 splits
-cv = StratifiedKFold(n_folds=num_folds,y=y)
+cv = StratifiedKFold(n_folds=num_folds, y=y)
 
 logging.info('Cross validation set up.')
 
 # 3. CREATE AND FIT MODEL
 # Define SpaceNetClassifier with tv-l1/graph-net penalty
-decoder = SpaceNetClassifier(cv=cv, memory="nilearn_cache", penalty=fitpenalty,
-                            memory_level=2, n_jobs=1, verbose=1, screening_percentile=screenp, mask=mnimask)
+decoder = SpaceNetClassifier(
+    cv=cv,
+    memory="nilearn_cache",
+    penalty=fitpenalty,
+    memory_level=2,
+    n_jobs=1,
+    verbose=1,
+    screening_percentile=screenp,
+    mask=mnimask)
 logging.info('Classifier set up. Starting model fit.')
 decoder.fit(X, y)  # fit
 logging.info('Model fit complete. Saving outputs...')
@@ -76,15 +85,19 @@ logging.info('Model fit complete. Saving outputs...')
 # 4. OUTPUTS
 
 # Save decoder object
-decoder_dir = './decoder/' # directory
-if not os.path.exists(decoder_dir): os.makedirs(decoder_dir) # create if missing
-dec_filename = '%s%s_decoder_SpaceNet_%sscreen%d_day%d_%s.jbl' %(decoder_dir,timestamp, decoder.penalty, screenp, testday, maskname)
+decoder_dir = './decoder/'  # directory
+if not os.path.exists(decoder_dir):
+    os.makedirs(decoder_dir)  # create if missing
+dec_filename = '%s%s_decoder_SpaceNet_%sscreen%d_day%d_%s.jbl' % (
+    decoder_dir, timestamp, decoder.penalty, screenp, testday, maskname)
 joblib.dump(decoder, dec_filename)
 
 # Save coefficients to nifti file
-coef_dir = './coefs/' # directory
-if not os.path.exists(coef_dir): os.makedirs(coef_dir) # create if missing
-coef_filename = '%s%s_coefs_SpaceNet_%sscreen%d_day%d_%s.nii' %(coef_dir,timestamp, decoder.penalty, screenp, testday, maskname)
+coef_dir = './coefs/'  # directory
+if not os.path.exists(coef_dir):
+    os.makedirs(coef_dir)  # create if missing
+coef_filename = '%s%s_coefs_SpaceNet_%sscreen%d_day%d_%s.nii' % (
+    coef_dir, timestamp, decoder.penalty, screenp, testday, maskname)
 coef_img = decoder.coef_img_
 coef_img.to_filename(coef_filename)
 logging.info('All outputs saved.')
